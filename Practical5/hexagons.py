292b64d51d5c6b8fb6fb1ed49b54e0f443733d67
GCB3220 <112191898@qq.com>
2023-03-18 12:58:26 +0800

    “Initialise”

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -1,3 +1,7 @@
+#aim to list five values of hexagon sequence
+#according to the formula calculate number and list
+#print
+#repeat until list five numbers
 z = 1 # how many hexagons
 for z in range (1,6): # 1 to 5
     x = 2 * z * (2 * z - 1) / 2
@@ -9,8 +13,7 @@ print ("over")
 
 
 # used to calculate h
-b = 1
-while b == 1: # continue use
+while 1 == 1: # continue use
     n = eval(input("n = "))
     h = 2*n*(2*n-1)/2 # calculate h
     print(h) # answer
