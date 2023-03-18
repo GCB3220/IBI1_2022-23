2fb6c477ea59cf8537fff32bf1d9ea665f505343
GCB3220 <112191898@qq.com>
2023-03-18 13:02:35 +0800

    “Initialise”

--- Practical5/mystery_code.py
+++ Practical5/mystery_code.py
@@ -1,14 +1,14 @@
 # ANSWER:give n a random number between 1 to 100.
-# if it is lager than number stoerd in the stored_random_number(the origin number is 0)
+# if it is lager than number stoerd in the stored_random_number
 # the number in n would be stored in the stored_random_number
 # this process continues 10 times(0 to 9)
 
 progress=0
 stored_random_number=0
-while progress<10:
+while progress<10: #repeat 10 times
 	progress+=1
-	n = randint(1,100)
+	n = randint(1,100) #random number
 	if n > stored_random_number:
-		stored_random_number = n
+		stored_random_number = n #store lager number
 
 print(stored_random_number)
