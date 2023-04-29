130a9db75b7b0990bf987acf56a6efea5832ae5b
GCB3220 <127167151+GCB3220@users.noreply.github.com>
2023-04-29 20:07:47 +0800

    Add files via upload

---
+++ Pratical11/Alignment.py
@@ -0,0 +1,25 @@
+import blosum as bl
+bl62 = bl.BLOSUM(62)
+
+def bl(name1, name2):
+    point = 0
+    amino = 0
+    protein1 = open('ACE2_%s.fa'%name1).read()
+    seq1 = protein1.split()[-1]
+
+    protein2 = open('ACE2_%s.fa'%name2).read()
+    seq2 = protein2.split()[-1]
+
+    for i in range(len(seq1)):
+        point = point+bl62[seq1[i]][seq2[i]]
+        if seq1[i] == seq2[i]:
+            amino = amino+1
+    percent = amino/len(seq1)
+
+    print("protein are:\n", protein1, protein2, "\n",
+          "alignment score :", point, "\n",
+          "percent of identical amino acid: ", percent)
+
+bl('mouse', 'cat')
+bl('mouse', 'human')
+bl('human', 'cat')
