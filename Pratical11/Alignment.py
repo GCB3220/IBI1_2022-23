d148e28ee218be5b72064c717377c35d3ba6bc1b
GCB3220 <127167151+GCB3220@users.noreply.github.com>
2023-04-29 20:11:09 +0800

    Add files via upload

--- Pratical11/Alignment.py
+++ Pratical11/Alignment.py
@@ -1,18 +1,18 @@
 import blosum as bl
-bl62 = bl.BLOSUM(62)
+bl62 = bl.BLOSUM(62) # BLOSUM62 matrix
 
 def bl(name1, name2):
     point = 0
     amino = 0
     protein1 = open('ACE2_%s.fa'%name1).read()
-    seq1 = protein1.split()[-1]
+    seq1 = protein1.split()[-1] # get amino acids
 
     protein2 = open('ACE2_%s.fa'%name2).read()
     seq2 = protein2.split()[-1]
 
     for i in range(len(seq1)):
-        point = point+bl62[seq1[i]][seq2[i]]
-        if seq1[i] == seq2[i]:
+        point = point+bl62[seq1[i]][seq2[i]] # get score
+        if seq1[i] == seq2[i]: # count number of identical amino acids
             amino = amino+1
     percent = amino/len(seq1)
 
