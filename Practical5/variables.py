9c2ee04b0a4b1433021e95fcd7f934539356249b
GCB3220 <112191898@qq.com>
2023-05-05 16:13:25 +0800

    “Initialise”

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -2,13 +2,13 @@ a = -3.19 #Edinburgh
 b = -118.24 #Los Angeles
 c = 116.39 #Haining
 d = a-b
-e = a-c
+e = c-a
 if d < e:
   print("Los Angeles is closer")
 elif d > e:
   print("haining is closer")
 else: print("same")
-# d>e Rob is closer to Haining
+# d>e Rob is closer to Los Angeles
 
 X = True
 Y = False
@@ -16,7 +16,7 @@ W = X and Y
 Z = X or Y
 print (W, Z)
 # W is true. Z is false
-input ()
+
 
 
 
