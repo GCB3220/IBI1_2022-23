9fca722473129451a7d58f7a82a655cca5be2d97
GCB3220 <112191898@qq.com>
2023-05-05 17:35:40 +0800

    “Initialise”

--- Practical6/flim.py
+++ Practical6/flim.py
@@ -3,13 +3,15 @@ flim = {'comdey': 73, 'action': 42, 'romance': 38, 'fantasy': 28, 'science-ficti
         'horror': 19, 'crime': 18, 'documentary': 12, 'history': 8, 'war': 7}
 print(flim)
 
+# print the number of students for which this genre is their favourite.
+favourite = 'comdey'
+print(flim[favourite])
 
 # draw a pie chart
 import matplotlib.pyplot as plt
 explode = [0, 0, 0, 0, 0, 0, 0, 0.1, 0.2, 0.3]
 plt.pie(list(flim.values()), # size
-        explode=explode, labels=list(flim.keys()), # use dictionary
-        autopct='%1.1f%%', pctdistance=0.7, shadow=False, startangle=90)
+        labels=list(flim.keys()), # use dictionary
+        explode=explode, autopct='%1.1f%%', pctdistance=0.7) # let the movie genres and percentages more clear
 plt.show()
-
 
